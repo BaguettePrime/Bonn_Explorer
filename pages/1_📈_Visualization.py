a866682e0ae4e168f46557789aa846f5230ee2cc
BaguettePrime <149872606+BaguettePrime@users.noreply.github.com>
2024-07-22 21:44:32 +0200

    Update 1_📈_Visualization.py

--- pages/1_📈_Visualization.py
+++ pages/1_📈_Visualization.py
@@ -3,6 +3,15 @@ import pandas as pd
 import os
 import plotly.express as px
 
+dfA = pd.read_csv("./data/setA.csv")
+dfB = pd.read_csv("./data/setB.csv")
+dfC = pd.read_csv("./data/setC.csv")
+dfD = pd.read_csv("./data/setD.csv")
+dfE = pd.read_csv("./data/setE.csv")
+
+# create a dictionary to store the dataframes
+dfs = {'DFA': dfA, 'DFB': dfB, 'DFC': dfC, 'DFD': dfD, 'DFE': dfE}
+
 # create a selectbox widget to select the dataframe
 selected_df = st.selectbox('Select a dataframe:', list(dfs.keys()),key = 1)
 
