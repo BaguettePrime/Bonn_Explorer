587adf643c0777a66bbb4a427b52bb8b73b70501
BaguettePrime <149872606+BaguettePrime@users.noreply.github.com>
2024-07-22 11:35:20 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,21 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+# Create a Streamlit app
+st.title("🎈 Bonn Explorer")
+
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+# Section 1: Data Loading
+st.header("Data Loading")
+
+#setA_url = https://raw.githubusercontent.com/BaguettePrime/Bonn_Explorer/main/data/setA.csv
+#response = requests.get(url)
+
+df = pd.read_csv("./data/setA.csv")
+
+
+#df.to_csv('setA.csv', sep=',', index=False, encoding='utf-8')
+
+print(df)
