45352deb38432370f7eac8e370935f8b5b999265
BaguettePrime <149872606+BaguettePrime@users.noreply.github.com>
2024-07-22 21:43:31 +0200

    Update 1_📈_Visualization.py

--- pages/1_📈_Visualization.py
+++ pages/1_📈_Visualization.py
@@ -1,3 +1,8 @@
+import streamlit as st
+import pandas as pd
+import os
+import plotly.express as px
+
 # create a selectbox widget to select the dataframe
 selected_df = st.selectbox('Select a dataframe:', list(dfs.keys()),key = 1)
 
