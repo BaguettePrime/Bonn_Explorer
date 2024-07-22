9fe713caca01095b6bece4928d42f16ba91ebf20
BaguettePrime <149872606+BaguettePrime@users.noreply.github.com>
2024-07-22 21:42:36 +0200

    Update 1_📈_Visualization.py

--- pages/1_📈_Visualization.py
+++ pages/1_📈_Visualization.py
@@ -1,1 +1,6 @@
+# create a selectbox widget to select the dataframe
+selected_df = st.selectbox('Select a dataframe:', list(dfs.keys()),key = 1)
+
+with st.expander(selected_df, expanded=False):
+    st.write(dfs[selected_df])
 
