8faadbfb4788616ed6f1ba981a1a2cd285889aba
BaguettePrime <149872606+BaguettePrime@users.noreply.github.com>
2024-07-22 11:51:12 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import os
 
 # Create a Streamlit app
 st.title("🎈 Bonn Explorer")
@@ -14,9 +15,27 @@ st.header("Data Loading")
 #setA_url = https://raw.githubusercontent.com/BaguettePrime/Bonn_Explorer/main/data/setA.csv
 #response = requests.get(url)
 
-df = pd.read_csv("./data/setA.csv")
+dfA = pd.read_csv("./data/setA.csv")
+dfB = pd.read_csv("./data/setB.csv")
+dfC = pd.read_csv("./data/setC.csv")
+dfD = pd.read_csv("./data/setD.csv")
+dfE = pd.read_csv("./data/setE.csv")
+
+# create a dictionary to store the dataframes
+dfs = {'DFA': dfA, 'DFB': dfB, 'DFC': dfC, 'DFD': dfD, 'DFE': dfE}
 
 
 #df.to_csv('setA.csv', sep=',', index=False, encoding='utf-8')
+#st.dataframe(df)
+#print(df)
+# create a selectbox widget to select the dataframe
+selected_df = st.selectbox('Select a dataframe:', list(dfs.keys()))
 
-print(df)
+with st.expander(selected_df, expanded=False):
+    st.write(dfs[selected_df])
+
+
+#st.dataframe(selected_df)
+
+# Section 1: Data Vizualisation
+st.header("Data Vizualisation")
