79ddc84503231ef69b70a2b3306f7f86f4fa7a3e
BaguettePrime <149872606+BaguettePrime@users.noreply.github.com>
2024-07-23 19:41:09 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -64,11 +64,6 @@ Let's get started!
 # Section 1: Data Loading
 st.header("Dataset inspection and description")
 
-#setA_url = https://raw.githubusercontent.com/BaguettePrime/Bonn_Explorer/main/data/setA.csv
-#response = requests.get(url)
-
-
-
 dfA = pd.read_csv("./data/setA.csv")
 dfB = pd.read_csv("./data/setB.csv")
 dfC = pd.read_csv("./data/setC.csv")
@@ -77,32 +72,6 @@ dfE = pd.read_csv("./data/setE.csv")
 
 # create a dictionary to store the dataframes
 dfs = {'DFA': dfA, 'DFB': dfB, 'DFC': dfC, 'DFD': dfD, 'DFE': dfE}
-
-#df.to_csv('setA.csv', sep=',', index=False, encoding='utf-8')
-#st.dataframe(df)
-#print(df)
-# create a selectbox widget to select the dataframe
-
-#st.dataframe(selected_df)
-
-# Section 1: Data Vizualisation
-st.header("Data Vizualisation")
-# create a selectbox widget to select the dataframe
-#@st.cache_data
-selected_df1 = st.selectbox('Select a dataframe:', list(dfs.keys()),key = 2)
-selected_df2 = dfs[selected_df1]
-
-
-
-# create a selectbox widget to select the column
-column_names = selected_df2.columns.tolist()
-selected_column = st.selectbox('Select a column:', column_names)
-
-# create a figure using plotly express
-fig = px.line(selected_df2, x=selected_df2.index, y=selected_column)
-
-# display the figure
-st.plotly_chart(fig, use_container_width=True)
 
 st.image('data/Table.png', caption='Sunrise by the mountains')
 
