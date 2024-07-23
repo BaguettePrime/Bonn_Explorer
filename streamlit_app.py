bb443fd1cee4c09aa585edfa07f736918465dcb0
BaguettePrime <149872606+BaguettePrime@users.noreply.github.com>
2024-07-23 18:50:16 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ st.set_page_config(
 st.title("🧠 Bonn Explorer")
 
 st.write(
-    "Let's start exploring! /n Everything you need is (almost) here: [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Let's start exploring! \n Everything you need is (almost) here: [docs.streamlit.io](https://docs.streamlit.io/)."
 )
 
 # Section 1: Introduction
