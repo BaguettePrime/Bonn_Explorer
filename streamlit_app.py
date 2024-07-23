a0986db637b0ac34bcef5b50a5c139ee4128a50f
BaguettePrime <149872606+BaguettePrime@users.noreply.github.com>
2024-07-23 19:18:47 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -82,11 +82,6 @@ dfs = {'DFA': dfA, 'DFB': dfB, 'DFC': dfC, 'DFD': dfD, 'DFE': dfE}
 #st.dataframe(df)
 #print(df)
 # create a selectbox widget to select the dataframe
-selected_df = st.selectbox('Select a dataframe:', list(dfs.keys()),key = 1)
-
-with st.expander(selected_df, expanded=False):
-    st.write(dfs[selected_df])
-
 
 #st.dataframe(selected_df)
 
