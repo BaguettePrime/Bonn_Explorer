9a8f56d9d9b03b89a1bbe0d046cfbaf824ed327e
BaguettePrime <149872606+BaguettePrime@users.noreply.github.com>
2024-07-25 22:28:35 +0200

    Update 1_📈_Data metrics.py

--- pages/1_📈_Data metrics.py
+++ pages/1_📈_Data metrics.py
@@ -5,26 +5,19 @@ import plotly.express as px
 import numpy as np
 from scipy import stats
 
-
 dfA = pd.read_csv("./data/setA.csv")
 dfB = pd.read_csv("./data/setB.csv")
 dfC = pd.read_csv("./data/setC.csv")
 dfD = pd.read_csv("./data/setD.csv")
 dfE = pd.read_csv("./data/setE.csv")
 
-# create a dictionary to store the dataframes
 dfs = {'Set A - Volunteers - Open Eyes': dfA, 'Set B - Volunteers - Closed Eyes': dfB, 'Set C - Patients(epi_zone) - Seizure free': dfC, 'Set D - Patients(Non_epi_zone) - Seizure free': dfD, 'Set E - Patients(epi_zone) - Seizure activity': dfE}
-
 
 # create a selectbox widget to select the dataframe
 selected_df = st.selectbox('Select a dataframe:', list(dfs.keys()),key = 1)
 selected_df1 = dfs[selected_df]
 column_names = selected_df1.columns.tolist()
 selected_column = st.selectbox('Select a column:', column_names)
-
-
-
-
 
 # Data Quality Metrics
 st.header("Data Quality")
@@ -37,11 +30,10 @@ st.write(dfs[selected_df].describe())
 
 # Data Distribution
 st.header("Data Distribution")
-#fig = px.histogram(dfs[selected_df], x=dfs[selected_df].columns[0], nbins=25)
 
 fig = px.histogram(dfs[selected_df], x=selected_column, nbins=25)
 
-with st.expander(selected_df, expanded=False):
+with st.expander(selected_df, expanded=True):
     st.plotly_chart(fig, use_container_width=True)
 
 fig1 = px.box(selected_df1, x=selected_column)
@@ -54,16 +46,8 @@ fig2 = px.scatter(selected_df1, x=selected_column, y=selected_df1.columns[1])
 with st.expander(selected_df, expanded=False):
     st.plotly_chart(fig2, use_container_width=True)
 
-
-
-
-
-
-# Calculate metrics
 #skewness = stats.skew(dfs[selected_df])
 #kurtosis = stats.kurtosis(dfs[selected_df])
-
-# Display metrics
 #st.write("Skewness:", skewness)
 #st.write("Kurtosis:", kurtosis)
 
