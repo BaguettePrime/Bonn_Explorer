ee190d7b413f0b5eb53c7cef92844f63509047e0
BaguettePrime <149872606+BaguettePrime@users.noreply.github.com>
2024-07-25 22:12:12 +0200

    Update 1_📈_Data metrics.py

--- pages/1_📈_Data metrics.py
+++ pages/1_📈_Data metrics.py
@@ -18,6 +18,13 @@ dfs = {'Set A - Volunteers - Open Eyes': dfA, 'Set B - Volunteers - Closed Eyes'
 
 # create a selectbox widget to select the dataframe
 selected_df = st.selectbox('Select a dataframe:', list(dfs.keys()),key = 1)
+selected_df1 = dfs[selected_df]
+column_names = selected_df1.columns.tolist()
+selected_column = st.selectbox('Select a column:', column_names)
+
+
+
+
 
 # Data Quality Metrics
 st.header("Data Quality")
